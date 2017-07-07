159f6cfee07c8a30e882e541ff4ea8366d9dc129
Justin Glenn Smith <noisesmith@gmail.com>
2017-07-06 17:07:26 -0700

    separating per step actions from stepper

--- src/clj/org/noisesmith/ludic_next.clj
+++ src/clj/org/noisesmith/ludic_next.clj
@@ -53,20 +53,40 @@
   (clock [this]
     t))
 
+(defn one-step
+  [game callback spy]
+  (let [next-game (delay (-> game
+                             (activate)
+                             (ludic/tick)
+                             (doto ludic/fire)
+                             (spy ::cycle)))]
+    (cond
+      (not (callback game ::cycle))
+      [::done game]
+      (= (ludic/clock game)
+         (ludic/clock (force next-game)))
+      [::continue game]
+      :else
+      [::incomplete (force next-game)])))
+
 (defn step-until-idle
   "iterates fire / tick until no progress is possible"
   [game spy callback]
-  (if-not (callback game ::cycle)
-    game
-    (let [next-game (-> game
-                        (activate)
-                        (ludic/tick)
-                        (doto ludic/fire)
-                        (spy ::cycle))]
-      (if (= (ludic/clock game)
-             (ludic/clock next-game))
-        game
-        (recur next-game spy callback)))))
+  (let [[status next-game] (one-step game callback spy)]
+    (case status
+      ::done [::done next-game]
+      ::incomplete (recur next-game spy callback)
+      ::continue [::continue next-game])))
+
+(defn main-pass
+  [game get-command callback dispatch spy]
+  (let [command (get-command game)]
+    (if-not (callback game command)
+      [::done game]
+      [::continue (-> game
+                      (spy ::pre-dispatch command)
+                      (ludic/transition dispatch command)
+                      (spy ::post-dispatch command))])))
 
 (defn main-loop
   ([game get-command dispatch]
@@ -74,14 +94,17 @@
   ([game get-command dispatch callback]
    (let [spy (fn spy [& context]
                (apply ludic/spy (:debug game) context))
-         command (get-command game)]
-     (if-not (callback game command)
-       game
-       (recur (-> game
-                  (spy ::pre-dispatch command)
-                  (ludic/transition dispatch command)
-                  (spy ::post-dispatch command)
-                  (step-until-idle spy callback))
+         [status next-state] (main-pass game get-command callback dispatch spy)
+         norm (delay (step-until-idle next-state spy callback))
+         norm-status (delay (first (force norm)))
+         normalized (delay (second (force norm)))]
+     (cond
+       (= status ::done)
+       next-state
+       (= (force norm-status) ::done)
+       (force normalized)
+       :else
+       (recur (force normalized)
               get-command
               dispatch
               callback)))))
